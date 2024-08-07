bb2c92bce032db2015d8e9f6d44f4504283da06e
Beansprout26 <107769774+Beansprout26@users.noreply.github.com>
2024-08-07 09:34:20 -0600

    html, css, js
    added basic styles and created players and game objects

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log('hi');
+const playerOne = {
+    name: 'Player One',
+    marker: "X"
+};
+
+
+const playerTwo = {
+    name: "Player Two",
+    marker: "O"
+};
+
+const game = [];
+
+
+
+// const gameBoard = document.getElementsByClassName('.gameContainer');
+
